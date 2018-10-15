6c5f365ae1a08aced20766c5aafc6c60f2932c66
Rob Calcote <robcalcote@gmail.com>
2018-10-15 15:19:35 -0600

    Forgot to save file, Added another commit to include changed file

--- ModelFun2/Models/GuestResponse.cs
+++ ModelFun2/Models/GuestResponse.cs
@@ -7,5 +7,9 @@ namespace ModelFun2.Models
 {
     public class GuestResponse
     {
+        // Data annotation
+        // Allows you to control inputs from users. This one below sets field as required and displays error message if left blank!
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
     }
 }
