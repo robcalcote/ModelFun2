5efeecc70059b8a28c96039399042e0ce4788e6a
Rob Calcote <robcalcote@gmail.com>
2018-10-17 13:37:27 -0600

    Added the rest of the variables and error messages if left blank

--- ModelFun2/Models/GuestResponse.cs
+++ ModelFun2/Models/GuestResponse.cs
@@ -9,7 +9,20 @@ namespace ModelFun2.Models
     {
         // Data annotation
         // Allows you to control inputs from users. This one below sets field as required and displays error message if left blank!
+
+        // Name : String
         [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+        // Email : String
+        [Required(ErrorMessage = "Please enter your email")]
+        public string Email { get; set; }
+
+        // Phone Number : String
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        // Attendance (null, yes, no) : bool
+        public bool? WillAttend { get; set; }
     }
 }
